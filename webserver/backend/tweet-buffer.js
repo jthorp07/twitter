999468b273238d8497d9dbb4db57efb6e55b6dd3
jathorp <jackthorp07@gmail.com>
2022-05-16 16:28:10 -0400

    TweetBuffer refactored from typedef & function into class

--- webserver/backend/tweet-buffer.js
+++ webserver/backend/tweet-buffer.js
@@ -1,60 +1,5 @@
-module.exports = {
+class TweetBuffer {
 
-    /**
-     * @typedef {Object} TweetData
-     * @property {string} id
-     * @property {string} text  
-     */
-
-    /**
-     * @typedef {Object} MatchingRuleData
-     * @property {string} id
-     * @property {string} tag
-     */
-
-    /**
-     * @typedef {Object} Tweet
-     * @property {TweetData} data
-     * @property {MatchingRuleData[]} matching_rules
-     */
-
-    /**
-     * @param {string} id
-     * @param {string} text
-     * @param {MatchingRuleData[]} matches
-     * 
-     * @returns {Tweet}
-     */
-    Tweet(id, text, matches) {
-        return {
-            data: {
-                id: id,
-                text: text
-            },
-            matching_rules: matches
-        }
-    },
-    
-    /**
-     * @callback TypeFnAddTweet
-     * @param {Tweet} tweet
-     * @returns {void}
-     */
-
-    /**
-     * @callback TypeFnGetTweets  
-     * @returns {Tweet[]}
-     */
-
-    /**
-     * @typedef {Object} TweetBuffer
-     * @property {Tweet[]} buffer
-     * @property {number} maxSize
-     * 
-     * @property {TypeFnAddTweet} add
-     * @property {TypeFnGetTweets} getTweets
-     * 
-     */
 
     /**
      * A simple queue-like data structure to manage
@@ -65,23 +10,60 @@ module.exports = {
      * device's hard drive
      * 
      * @param {number} maxSize Maximum number of Tweets that can be stored
-     * @returns {TweetBuffer}
      */
-    TweetBuffer(maxSize) {
-        return {
-            buffer: [],
-            maxSize: maxSize,
-            add(tweet) {
-                this.buffer.push(tweet);
-                // If over capacity, remove first (oldest) element
-                if (this.buffer.length > maxSize) {
-                    this.buffer.splice(0,1);
-                }
-            },
-            getTweets() {
-                return this.buffer;
-            }
+    constructor(maxSize) {
+
+        /** @type {Tweet[]} */
+        this.buffer = [];
+        this.maxSize = maxSize;
+
+    }
+
+
+    add(tweet) {
+        this.buffer.push(tweet);
+        // If over capacity, remove first (oldest) element
+        if (this.buffer.length > maxSize) {
+            this.buffer.splice(0, 1);
         }
     }
 
+    getTweets() {
+        return this.buffer;
+    }
+
+}
+
+
+
+/**
+ * @typedef {Object} MatchingRuleData
+ * @property {string} id
+ * @property {string} tag
+ */
+
+
+class Tweet {
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {string} text 
+     * @param {MatchingRuleData[]} matches 
+     */
+    constructor(id, text, matches) {
+
+        this.data = {
+            id: id,
+            text: text
+        }
+
+        this.matching_rules = matches;
+
+    }
+}
+
+module.exports = {
+    TweetBuffer,
+    Tweet
 }
